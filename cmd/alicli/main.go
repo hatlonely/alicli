17144cad3b8f1e102396085a1d80b5deca966e47
hatlonely <hatlonely@gmail.com>
2020-04-19 18:39:24 +0800

    update

--- cmd/alicli/main.go
+++ cmd/alicli/main.go
@@ -2,55 +2,48 @@ package main
 
 import (
 	"fmt"
-	"github.com/hpifu/go-kit/hconf"
-	"github.com/hpifu/go-kit/hdef"
-	"github.com/hpifu/go-kit/henv"
-	"github.com/hpifu/go-kit/hflag"
-	"github.com/hpifu/go-kit/hrule"
 	"os"
-)
 
-type Options struct {
-	AccessKeyID     string
-	AccessKeySecret string
-}
+	"github.com/hatlonely/alicli/internal/workflow"
+	"github.com/hpifu/go-kit/hconf"
+	"github.com/hpifu/go-kit/hflag"
+)
 
 var AppVersion = "unknown"
 
+
+type Options struct {
+	CtxFile string `hflag:"--ctx-file, -c; usage: context file path"`
+	WorkFile string `hflag:"--work-file, -w; required; usage: work file path"`
+}
+
 func main() {
 	version := hflag.Bool("v", false, "print current version")
-	configfile := hflag.String("c", "configs/alicli.yaml", "config file path")
+	options := &Options{}
 
-	if err := hflag.Bind(&Options{}); err != nil {
+	if err := hflag.Bind(options); err != nil {
 		panic(err)
 	}
 	if err := hflag.Parse(); err != nil {
+		fmt.Println(hflag.Usage())
 		panic(err)
 	}
 	if *version {
 		fmt.Println(AppVersion)
 		os.Exit(0)
 	}
-	options := &Options{}
-	if err := hdef.SetDefault(options); err != nil {
-		panic(err)
-	}
-	config, err := hconf.New("yaml", "local", *configfile)
+
+	ctxConfig, err := hconf.New("yaml", "local", options.CtxFile)
 	if err != nil {
 		panic(err)
 	}
-	if err := config.Unmarshal(options); err != nil {
-		panic(err)
-	}
-	if err := henv.NewHEnv("ALICLI").Unmarshal(options); err != nil {
-		panic(err)
-	}
-	if err := hflag.Unmarshal(options); err != nil {
-		panic(err)
-	}
-	if err := hrule.Evaluate(options); err != nil {
+	kvs := map[string]string{}
+	if err := ctxConfig.Unmarshal(&kvs); err != nil {
 		panic(err)
 	}
 
-	fmt.Println(options)
+	ctx := workflow.NewCtx()
+	for k, v := range kvs {
+		ctx.Set(fmt.Sprintf("global.%v", k), v)
+	}
 }
