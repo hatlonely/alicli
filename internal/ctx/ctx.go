c72bd6bb2e82975c185b0569d1c054f8756587ce
贺乐 <hatlonely.hl@alibaba-inc.com>
2020-04-20 23:44:20 +0800

    update

--- internal/ctx/ctx.go
+++ internal/ctx/ctx.go
@@ -1,6 +1,8 @@
 package ctx
 
 import (
+	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/hpifu/go-kit/hconf"
@@ -29,4 +31,65 @@ func (c *Ctx) Set(key string, val interface{}) error {
 	defer c.mutex.Unlock()
 
 	return c.store.Set(key, val)
+}
+
+func (c *Ctx) Evaluate(data *interface{}) (interface{}, error) {
+	switch (*data).(type) {
+	case string:
+		v := (*data).(string)
+		if strings.HasPrefix(v, "{{") && strings.HasSuffix(v, "}}") {
+			text := v[2 : len(v)-2]
+			if strings.HasPrefix(text, "type.int64(") {
+				val, err := strconv.ParseInt(text[11:len(text)-1], 10, 64)
+				if err != nil {
+					return nil, err
+				}
+				*data = val
+			} else {
+				key := strings.TrimSpace(text)
+				val, err := c.Get(key)
+				if err != nil {
+					return nil, err
+				}
+				*data = val
+			}
+		}
+	case map[string]interface{}:
+		for k, v := range (*data).(map[string]interface{}) {
+			if val, err := c.Evaluate(&v); err != nil {
+				return nil, err
+			} else {
+				(*data).(map[string]interface{})[k] = val
+			}
+		}
+	case map[interface{}]interface{}:
+		for k, v := range (*data).(map[interface{}]interface{}) {
+			if val, err := c.Evaluate(&v); err != nil {
+				return nil, err
+			} else {
+				(*data).(map[interface{}]interface{})[k] = val
+			}
+		}
+	case []string:
+		for i, v := range (*data).([]string) {
+			var vi interface{}
+			vi = v
+			if val, err := c.Evaluate(&vi); err != nil {
+				return nil, err
+			} else {
+				(*data).([]interface{})[i] = val
+			}
+		}
+	case []interface{}:
+		for i, v := range (*data).([]interface{}) {
+			if val, err := c.Evaluate(&v); err != nil {
+				return nil, err
+			} else {
+				(*data).([]interface{})[i] = val
+			}
+		}
+	default:
+	}
+
+	return *data, nil
 }
